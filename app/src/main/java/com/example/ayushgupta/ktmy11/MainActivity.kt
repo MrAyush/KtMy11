71d1cd5906e86948d0800a656c0895d98123e1e2
HATERS\Ayush Gupta <ayushkgupta1998@gmail.com>
2018-06-04 17:34:21 +0530

    MediaPlayer made

--- app/src/main/java/com/example/ayushgupta/ktmy11/MainActivity.kt
+++ app/src/main/java/com/example/ayushgupta/ktmy11/MainActivity.kt
@@ -1,22 +1,68 @@
 package com.example.ayushgupta.ktmy11
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.widget.ListView
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     private var songs: ArrayList<Songs>? = null
     private var lview: ListView? = null
+    private var rl1: RelativeLayout? = null
+    private var media: MediaPlayer? = null
+    private var song: Songs? = null
+    private var iv4: ImageView? = null
+    private var tv3: TextView? = null
+    private var tv4: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rl1 = findViewById(R.id.rl1)
+        iv4 = findViewById(R.id.iv4)
+        tv3 = findViewById(R.id.tv3)
+        tv4 = findViewById(R.id.tv4)
+
         songs = ArrayList()
         getSong()
+
         lview = findViewById(R.id.lview)
-        lview?.adapter = MyAdapter(songs!!, this)
+        val adapter = MyAdapter(songs!!, this)
+        lview?.adapter = adapter
+        lview?.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            song = adapter.getItem(position) as Songs
+            val uri = Uri.parse("file:///${song!!.path}")
+            iv4?.setImageResource(R.drawable.ic_pause_black_24dp)
+            rl1?.visibility = View.VISIBLE
+            tv3?.text = song?.name
+            tv4?.text = song?.author
+            if (media == null)
+                media = MediaPlayer.create(this@MainActivity, uri)
+            if (media?.isPlaying!!) {
+                media?.stop()
+                media = MediaPlayer.create(this@MainActivity, uri)
+            }
+            media?.start()
+            media?.setOnCompletionListener {
+                media?.stop()
+                media?.release()
+                media = null
+            }
+        }
+        iv4?.setOnClickListener {
+            if (media!!.isPlaying) {
+                media?.pause()
+                iv4?.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+            } else {
+                media?.start()
+                iv4?.setImageResource(R.drawable.ic_pause_black_24dp)
+            }
+        }
     }
 
     private fun getSong() {
--- app/src/main/java/com/example/ayushgupta/ktmy11/MyAdapter.kt
+++ app/src/main/java/com/example/ayushgupta/ktmy11/MyAdapter.kt
@@ -1,25 +1,20 @@
 package com.example.ayushgupta.ktmy11
 
-import android.media.MediaPlayer
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 
 class ViewHolder {
     var iv1: ImageView? = null
     var tv1: TextView? = null
     var tv2: TextView? = null
-    var iv2: ImageView? = null
     var path: String? = null
 }
 
 class MyAdapter(private val songs: ArrayList<Songs>, private val mActivity: MainActivity) : BaseAdapter() {
-    private var count = 1
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val layoutInflater = LayoutInflater.from(mActivity)
         val song = songs[position]
@@ -30,7 +25,6 @@ class MyAdapter(private val songs: ArrayList<Songs>, private val mActivity: Main
             holder.iv1 = view.findViewById(R.id.iv1)
             holder.tv1 = view.findViewById(R.id.tv1)
             holder.tv2 = view.findViewById(R.id.tv2)
-            holder.iv2 = view.findViewById(R.id.iv2)
             view.tag = holder
         } else {
             holder = view.tag as ViewHolder
@@ -38,15 +32,6 @@ class MyAdapter(private val songs: ArrayList<Songs>, private val mActivity: Main
         holder.tv1?.text = song.name
         holder.tv2?.text = song.author
         holder.path = song.path
-        val uri = Uri.parse("file:///${holder.path}")
-        val media = MediaPlayer.create(mActivity, uri)
-        holder.iv2?.setOnClickListener {
-            if (media.isPlaying){
-                media.pause()
-            }else{
-                media.start()
-            }
-        }
         return view!!
     }
 
