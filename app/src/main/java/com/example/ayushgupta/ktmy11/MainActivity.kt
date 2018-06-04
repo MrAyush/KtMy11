d459a7ed79c7b1b4e35f21b069e661d07a659956
HATERS\Ayush Gupta <ayushkgupta1998@gmail.com>
2018-06-04 23:52:02 +0530

    Permission handled

--- app/src/main/java/com/example/ayushgupta/ktmy11/MainActivity.kt
+++ app/src/main/java/com/example/ayushgupta/ktmy11/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.ayushgupta.ktmy11
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.*
 
@@ -18,49 +22,53 @@ class MainActivity : AppCompatActivity() {
     private var iv4: ImageView? = null
     private var tv3: TextView? = null
     private var tv4: TextView? = null
+    private var mStatus = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        rl1 = findViewById(R.id.rl1)
-        iv4 = findViewById(R.id.iv4)
-        tv3 = findViewById(R.id.tv3)
-        tv4 = findViewById(R.id.tv4)
-
         songs = ArrayList()
-        getSong()
+        getPermission()
 
-        lview = findViewById(R.id.lview)
-        val adapter = MyAdapter(songs!!, this)
-        lview?.adapter = adapter
-        lview?.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            song = adapter.getItem(position) as Songs
-            val uri = Uri.parse("file:///${song!!.path}")
-            iv4?.setImageResource(R.drawable.ic_pause_black_24dp)
-            rl1?.visibility = View.VISIBLE
-            tv3?.text = song?.name
-            tv4?.text = song?.author
-            if (media == null)
-                media = MediaPlayer.create(this@MainActivity, uri)
-            if (media?.isPlaying!!) {
-                media?.stop()
-                media = MediaPlayer.create(this@MainActivity, uri)
-            }
-            media?.start()
-            media?.setOnCompletionListener {
-                media?.stop()
-                media?.release()
-                media = null
-            }
-        }
-        iv4?.setOnClickListener {
-            if (media!!.isPlaying) {
-                media?.pause()
-                iv4?.setImageResource(R.drawable.ic_play_arrow_black_24dp)
-            } else {
-                media?.start()
+        if (mStatus) {
+            rl1 = findViewById(R.id.rl1)
+            iv4 = findViewById(R.id.iv4)
+            tv3 = findViewById(R.id.tv3)
+            tv4 = findViewById(R.id.tv4)
+
+            getSong()
+            lview = findViewById(R.id.lview)
+            val adapter = MyAdapter(songs!!, this)
+            lview?.adapter = adapter
+            lview?.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+                song = adapter.getItem(position) as Songs
+                val uri = Uri.parse("file:///${song!!.path}")
                 iv4?.setImageResource(R.drawable.ic_pause_black_24dp)
+                rl1?.visibility = View.VISIBLE
+                tv3?.text = song?.name
+                tv4?.text = song?.author
+                if (media == null)
+                    media = MediaPlayer.create(this@MainActivity, uri)
+                if (media?.isPlaying!!) {
+                    media?.stop()
+                    media = MediaPlayer.create(this@MainActivity, uri)
+                }
+                media?.start()
+                media?.setOnCompletionListener {
+                    media?.stop()
+                    media?.release()
+                    iv4?.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+                    media = MediaPlayer.create(this@MainActivity, uri)
+                }
+            }
+            iv4?.setOnClickListener {
+                if (media!!.isPlaying) {
+                    media?.pause()
+                    iv4?.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+                } else {
+                    media?.start()
+                    iv4?.setImageResource(R.drawable.ic_pause_black_24dp)
+                }
             }
         }
     }
@@ -79,5 +87,25 @@ class MainActivity : AppCompatActivity() {
         }
 
         cr.close()
+    }
+
+    private fun getPermission() {
+        val status = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        if (status == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 100)
+        } else {
+            mStatus = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            Toast.makeText(this, "Permission needed to continue", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            recreate()
+            mStatus = true
+        }
     }
 }
